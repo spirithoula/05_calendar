a85c9587b4286389fbd7c9c667546271ee8ec4e3
Alex Harrington <spirithoula2@gmail.com>
2021-04-17 20:43:18 -0500

    moved

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,3 +1,6 @@
 var scheduleContainerEl = $('#scheduleContainer');
+var currentDay = $('#currentDay');
+var currentDay = dayjs('04/17/21');
+$('#1a').text(currentDay.format('MMM Do, YYYY'));
 var schedule = {}
 localStorage.setItem('schedule', JSON.stringify(schedule))
